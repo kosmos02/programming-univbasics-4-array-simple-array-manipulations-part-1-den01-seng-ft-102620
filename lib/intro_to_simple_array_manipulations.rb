977ef34f91d2b6e60279f18a4621fcabf7fbcfee
Alexander Gabriel <alexandrgabe@gmail.com>
2020-09-26 17:29:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,4 +4,18 @@ def using_push(array, string)
 
 end
 
+def using_unshift(array, string)
+  
+  array.unshift(string)
+  
+end
 
+def using_pop(array)
+  
+  array.pop 
+  
+end
+
+def pop_with_args
+  
+  
